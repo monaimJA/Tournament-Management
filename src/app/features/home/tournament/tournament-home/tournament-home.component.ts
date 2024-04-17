ad72777c07c4453926a925c2b1dc3a4ca83a226b
elnasr <el-mehdi.nasr@capgemini.com>
2024-04-17 12:32:13 +0100

    comm

--- src/app/features/home/tournament/tournament-home/tournament-home.component.ts
+++ src/app/features/home/tournament/tournament-home/tournament-home.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
-import {InscriptionDialogComponent} from "../../inscription-dialog/inscription-dialog.component";
+import { InscriptionComponent } from '../../inscription/inscription.component';
+import { TournamentService } from 'src/app/core/services/tournament/tournament.service';
+import { Tournament } from 'src/app/core/models/Tournament';
 
 @Component({
   selector: 'app-tournament-home',
   templateUrl: './tournament-home.component.html',
   styleUrls: ['./tournament-home.component.css']
 })
-export class TournamentHomeComponent {
-  constructor() {
+export class TournamentHomeComponent implements OnInit{
+  currentTournament : Tournament;
+  constructor(private dialog:MatDialog,private tournamentSercive : TournamentService) {
+  }
+  ngOnInit(): void {
+    this.handleCurrentTournament();
+  }
+  openDialog(){
+
+    this.dialog.open(InscriptionComponent,{
+      width:'60%',
+      height:'400px'
+    })
+  }
+  handleCurrentTournament(){
+    this.tournamentSercive.getCurrentTournament()
+    .subscribe((data)=>{this.currentTournament =data;
+    } );
+  }
+  
+  inscriptionOk(): boolean {
+    return this.currentTournament.statusTournament=="INSCRIPTION"?true:false ;
   }
 }
